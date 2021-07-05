b878bf73a11f2a55a6d4c96ebf0c41d181514fc7
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-05 21:02:21 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+
+    <body>
+        <table> <?php 
+           for($i=0;$i<=8;$i++){
+               //print row 
+               <tr>
+               for($j=0;$j<=8;$j++){
+                   //print cols
+                   
+                   
+               }
+               </tr>
+           }
+        
+        ?>
+        </table>
+        <script src="" async defer>
+        </script>
+    </body>
+
+</html>
